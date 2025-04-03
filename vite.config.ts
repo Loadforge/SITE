fbafc3def78048e2db451f8ca9be3f907dc25347
Jean Lima <jeanlima5939@gmail.com>
2025-04-03 11:37:17 -0300

    Fix: Error in deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SITE/",
+  base: "/SITE",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
