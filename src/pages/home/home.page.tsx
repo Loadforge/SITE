3c61b59124f227de05b7ba8d3c5c60c659979845
Jean Lima <jeanlima5939@gmail.com>
2025-04-05 15:26:17 -0300

    Fix: Tailwind use Colors

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,10 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export function HomePage() {
   return (
-    <>
-      <div className="flex flex-col bg-background items-center justify-center min-h-svh ">
-        <Button>Click me</Button>
-      </div>
-    </>
+    <div className="flex bg-background items-center gap-2 justify-center min-h-svh">
+      <Button className="bg-background text-white hover:opacity-90">Background</Button>
+      <Button className="bg-fill text-white hover:opacity-90">Fill</Button>
+      <Button className="bg-primary text-black hover:opacity-90">Primary</Button>
+      <Button className="bg-secondary text-white hover:opacity-90">Secondary</Button>
+      <Button className="bg-accent text-white hover:opacity-90">Accent</Button>
+      <Button className="bg-muted text-white hover:opacity-90">Muted</Button>
+    </div>
   );
 }
