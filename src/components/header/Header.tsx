ad87d14ef0b50e4cf3b73ee369479e6b4575d2cb
Jean Lima <jeanlima5939@gmail.com>
2025-04-05 16:51:25 -0300

    Feat: add Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import Logo from '../../../public/Logo.svg';
+
+export function Header() {
+  return (
+    <header className="bg-background-secondary border-b text-text flex items-center px-50 h-16 justify-between fixed top-0 left-0 w-full">
+      <div className="flex items-end">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="max-h-full max-w-[90px] object-contain"
+        />
+      
+      </div>
+    </header>
+  );
+}
--- src/pages/listPage/list.page.tsx
+++ src/pages/listPage/list.page.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components";
 
 export function ListPage() {
   return (
     <div className="flex bg-background items-center gap-2 justify-center min-h-svh">
-      <Button className="bg-background text-white hover:opacity-90">Background</Button>
-      <Button className="bg-fill text-white hover:opacity-90">Fill</Button>
-      <Button className="bg-primary text-black hover:opacity-90">Primary</Button>
-      <Button className="bg-secondary text-white hover:opacity-90">Secondary</Button>
-      <Button className="bg-accent text-white hover:opacity-90">Accent</Button>
-      <Button className="bg-muted text-white hover:opacity-90">Muted</Button>
+      <Header />
     </div>
   );
 }
