11c120a05ec1503d7dc3411258f3a9157f563d8e
Jean Lima <jeanlima5939@gmail.com>
2025-04-05 23:48:07 -0300

    Feat: List Page

--- src/components/cards/project.card.tsx
+++ src/components/cards/project.card.tsx
@@ -1,13 +1,14 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { GripVertical, MoreVertical } from "lucide-react";
 import React from "react";
+import { IconType } from "react-icons/lib";
 
 interface ProjectCardProps {
-  nome: string;
-  icone: React.ReactNode;
+  title: string;
+  icon: IconType;
 }
 
-export function ProjectCard({ nome, icone }: ProjectCardProps) {
+export function ProjectCard({ title, icon }: ProjectCardProps) {
   return (
     <Card className="relative w-48 h-64 p-6 text-white rounded-xl shadow-md flex flex-col items-center justify-between overflow-hidden">
       <div className="absolute top-2 left-2 text-gray-400">
@@ -18,11 +19,11 @@ export function ProjectCard({ nome, icone }: ProjectCardProps) {
       </div>
 
       <CardContent className="flex flex-col flex-grow items-center justify-center pt-6">
-        <div className="text-8xl ">{icone}</div>
+        <div className="text-8xl ">{React.createElement(icon)}</div>
       </CardContent>
       <CardFooter>
         {" "}
-        <span className="text-lg font-semibold text-center">{nome}</span>
+        <span className="text-lg font-semibold text-center">{title}</span>
       </CardFooter>
     </Card>
   );
