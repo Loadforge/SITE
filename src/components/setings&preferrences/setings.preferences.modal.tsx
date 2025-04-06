fe19386af9978cd532fabd836b1062caa8a5a906
Jean Lima <jeanlima5939@gmail.com>
2025-04-06 17:32:08 -0300

    Fix: Responsive Desing

--- src/components/setings&preferrences/setings.preferences.modal.tsx
+++ src/components/setings&preferrences/setings.preferences.modal.tsx
@@ -1,26 +1,27 @@
 export type SettingsPreferencesModalProps = {
-    onClose: () => void;
-    isOpen: boolean;
+  onClose: () => void;
+  isOpen: boolean;
+};
+
+export function SettingsPreferencesModal({
+  onClose,
+  isOpen,
+}: SettingsPreferencesModalProps) {
+  if (!isOpen) return null;
+
+  const handleModalClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
   };
-  
-  export function SettingsPreferencesModal({ onClose, isOpen }: SettingsPreferencesModalProps) {
-    if (!isOpen) return null;
-  
-    return (
-      <div className="fixed top-0 left-0 w-full h-full bg-background-secondary bg-opacity-50 flex items-center justify-center z-50">
-        <div className="bg-white rounded-lg shadow-lg p-6 w-1/3 relative">
-          <button
-            onClick={onClose}
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-xl"
-          >
-            ×
-          </button>
-          <h2 className="text-2xl font-bold mb-4">Settings & Preferences</h2>
-          <p className="text-gray-700 mb-4">
-            Customize your settings and preferences here.
-          </p>
-        </div>
-      </div>
-    );
-  }
-  
+
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-background-home/70 flex mt-16 justify-center "
+      onClick={onClose}
+    >
+      <div
+        className="bg-background-secondary rounded-lg shadow-lg p-6 w-2/3 h-4/5 "
+        onClick={handleModalClick}
+      ></div>
+    </div>
+  );
+}
