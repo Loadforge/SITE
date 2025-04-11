bc6a74409a6ceee0fad4655ecdc089e14ac969d0
JeanLima2112 <jeanlima5939@gmail.com>
2025-04-11 19:15:14 -0300

    Feat: settings and preferences sidebar

--- src/components/setings&preferrences/setings.preferences.modal.tsx
+++ src/components/setings&preferrences/setings.preferences.modal.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import React from "react";
+import {
+  SettingsPreferencesTabs,
+  SettingsPreferencesTabsList,
+  SettingsPreferencesTabsTrigger,
+  SettingsPreferencesTabsContent,
+} from "./setings.preferences.tabs";
+import { Settings, SlidersHorizontal } from "lucide-react";
+
 export type SettingsPreferencesModalProps = {
   onClose: () => void;
   isOpen: boolean;
@@ -15,13 +26,40 @@ export function SettingsPreferencesModal({
 
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full bg-background-home/70 flex mt-16 justify-center "
+      className="fixed top-0 left-0 w-full h-full bg-background/70 flex justify-center items-start pt-16 "
       onClick={onClose}
     >
       <div
-        className="bg-background-secondary rounded-lg shadow-lg p-6 w-2/3 h-4/5 "
+        className="bg-background-secondary rounded-lg shadow-lg w-4/5 max-w-6xl h-[80vh] flex overflow-hidden"
         onClick={handleModalClick}
-      ></div>
+      >
+        <SettingsPreferencesTabs defaultValue="preferences">
+          <SettingsPreferencesTabsList>
+            <div className="space-y-2">
+              <SettingsPreferencesTabsTrigger value="preferences">
+                <SlidersHorizontal className="size-4" />
+                Preferences
+              </SettingsPreferencesTabsTrigger>
+              <SettingsPreferencesTabsTrigger value="settings">
+                <Settings className="size-4" />
+                Settings
+              </SettingsPreferencesTabsTrigger>
+            </div>
+
+            <div className="text-md flex justify-center text-muted-foreground pt-6 font-bold">
+              Version: 1.0.0
+            </div>
+          </SettingsPreferencesTabsList>
+
+          <SettingsPreferencesTabsContent value="preferences">
+            Conteúdo de Preferências
+          </SettingsPreferencesTabsContent>
+
+          <SettingsPreferencesTabsContent value="settings">
+            Conteúdo de Configurações
+          </SettingsPreferencesTabsContent>
+        </SettingsPreferencesTabs>
+      </div>
     </div>
   );
 }
---
+++ src/components/setings&preferrences/setings.preferences.tabs.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { cn } from "@/lib/utils";
+import { Settings, SlidersHorizontal } from "lucide-react";
+
+function SettingsPreferencesTabs({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn("flex flex-row h-full", className)}
+      {...props}
+    />
+  );
+}
+
+function SettingsPreferencesTabsList({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        "flex flex-col justify-between bg--background-secondary  border-r text-text w-60 py-4 pt-10",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function SettingsPreferencesTabsTrigger({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        "data-[state=active]:bg-separators data-[state=active]:text-white flex items-center gap-2 px-3 py-2 text-md font-semibold rounded-sm w-full hover:bg-background/50 transition-colors",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </TabsPrimitive.Trigger>
+  );
+}
+
+function SettingsPreferencesTabsContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn("flex-1 p-6 overflow-y-auto", className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  SettingsPreferencesTabs,
+  SettingsPreferencesTabsList,
+  SettingsPreferencesTabsTrigger,
+  SettingsPreferencesTabsContent,
+};
