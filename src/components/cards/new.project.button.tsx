b3e2dc525670d7e3bc630b4149487799721315f5
JeanLima2112 <jeanlima5939@gmail.com>
2025-04-11 17:22:12 -0300

    Feat: Button New-project function

--- src/components/cards/new.project.button.tsx
+++ src/components/cards/new.project.button.tsx
@@ -1,10 +1,17 @@
 import { FaPlus } from "react-icons/fa";
 import { Card } from "@/components/ui/card";
 
-export function NewProjectButton() {
+interface NewProjectButtonProps {
+  onClick?: () => void;
+}
+
+export function NewProjectButton({ onClick }: NewProjectButtonProps) {
   return (
-    <Card className="w-48 h-64 bg-primary rounded-xl border border-dashed border-gray-300 hover:border-white hover:opacity-80 transition-colors flex flex-col items-center justify-center text-white shadow-md cursor-pointer">
-      <FaPlus  size={48} className="mb-4" />
+    <Card
+      onClick={onClick}
+      className="w-48 h-64 bg-primary rounded-xl border border-dashed border-gray-300 hover:border-white hover:opacity-80 transition-colors flex flex-col items-center justify-center text-white shadow-md cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
+    >
+      <FaPlus size={48} className="mb-4" />
       <span className="text-xl font-semibold">New project</span>
     </Card>
   );
--- src/components/cards/project.card.tsx
+++ src/components/cards/project.card.tsx
@@ -12,9 +12,9 @@ interface ProjectCardProps {
 
 export function ProjectCard({ title, icon }: ProjectCardProps) {
   return (
-    <Card className=" w-48 h-64 p-6 text-white rounded-xl shadow-md flex flex-col items-center justify-between overflow-hidden">
+    <Card className=" w-48 h-64 p-6 text-white rounded-xl shadow-md flex flex-col items-center justify-between overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105">
       <div className=" w-full flex justify-between top-2 left-2 text-text">
-        <LuGrip size={16} />
+        <LuGrip size={16} className="cursor-grab" />
         <MoreVertical size={16} />
       </div>
 
