9d70053279d551ba1fb80685d6236126b1bfae1a
JeanLima2112 <jeanlima5939@gmail.com>
2025-04-24 19:24:13 -0300

    Fix Scroll in headers and Params

--- src/components/project/tabs/headers.req.tsx
+++ src/components/project/tabs/headers.req.tsx
@@ -1,4 +1,9 @@
+import { Trash2, Plus, Pencil } from "lucide-react";
 import { useState, KeyboardEvent, useRef } from "react";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/index";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableHeader,
@@ -7,10 +12,6 @@ import {
   TableHead,
   TableCell,
 } from "@/components/ui/table";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/index";
-import { Input } from "@/components/ui/input";
-import { Trash2, Plus, Pencil } from "lucide-react";
 
 type QueryParam = {
   id: string;
@@ -132,7 +133,7 @@ const handleAddParam = () => {
 
   return (
     <div className="space-y-4">
-    <div className="overflow-y-auto max-h-75">
+    <div className="overflow-y-auto max-h-[40vh]">
     <Table>
           <TableHeader>
             <TableRow>
