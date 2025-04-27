6ddac469f72748e884b17255c58a94b5a86a2536
JeanLima2112 <jeanlima5939@gmail.com>
2025-04-27 17:14:59 -0300

    Feat: add Tooltip

--- src/components/sidebar/navigation.tsx
+++ src/components/sidebar/navigation.tsx
@@ -2,7 +2,6 @@
 
 import { Plus } from "lucide-react";
 
-
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -12,10 +11,11 @@ import {
 } from "@/components/ui/sidebar";
 import { Request } from "@/db/types/request.type";
 import { useProjectStore } from "@/stores/project.store";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui";
 interface Props {
   requests: Request[];
   handleCreateRequest: () => void;
-
 }
 
 export function Navigation({ requests, handleCreateRequest }: Props) {
@@ -25,15 +25,29 @@ export function Navigation({ requests, handleCreateRequest }: Props) {
     <SidebarGroup>
       <SidebarGroupLabel className="flex items-center justify-between">
         Requests
-        <button className="text-muted-foreground hover:text-white">
-          <Plus onClick={handleCreateRequest} size={16} />
-        </button>
+        <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+        <button className="text-text hover:text-primary transition-all">
+            <Plus className="" onClick={handleCreateRequest} size={16} />
+          </button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Criar Requisição</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
       </SidebarGroupLabel>
+
+     
 
       <SidebarMenu>
         {requests?.map((req, index) => (
-          <SidebarMenuItem key={index}  >
-            <SidebarMenuButton asChild className="hover:bg-separators/40 hover:text-text">
+          <SidebarMenuItem key={index}>
+            <SidebarMenuButton
+              asChild
+              className="hover:bg-separators/40 hover:text-text"
+            >
               <a
                 href="#"
                 onClick={(e) => {
