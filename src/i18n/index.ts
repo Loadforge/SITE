9a13dc6d2bddf74017fae4b349f01882c4ae6b38
JeanLima2112 <jeanlima5939@gmail.com>
2025-05-04 17:49:29 -0300

    Fix: Language Refresh

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,18 +1,21 @@
-import i18n from 'i18next';
-import HttpBackend from 'i18next-http-backend';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import HttpBackend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+const savedLang = localStorage.getItem("appLanguage");
 
 i18n
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    lng: savedLang || "en", 
+    fallbackLng: "en",
     debug: true,
     interpolation: {
       escapeValue: false,
     },
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
+      loadPath: "/locales/{{lng}}/translation.json",
     },
   });
 
