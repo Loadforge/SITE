e1351e9720a0f2e92661413d5e38cb683b5dc549
JeanLima2112 <jeanlima5939@gmail.com>
2025-09-14 14:47:26 -0300

    feat: docs link

--- src/components/docs/docs.button.tsx
+++ src/components/docs/docs.button.tsx
@@ -1,13 +1,23 @@
 import { useTranslation } from "react-i18next";
 import { FaBook } from "react-icons/fa";
 
+import { ExternalLinkService } from "@/services";
+
 import { Button } from "../ui/button";
 
 export function DocsButton() {
   const { t } = useTranslation();
 
+  const handleClick = () => {
+    ExternalLinkService.openDocs();
+  };
+
   return (
-    <Button variant="homeButton" className="flex items-center gap-2">
+    <Button
+      variant="homeButton"
+      className="flex items-center gap-2"
+      onClick={handleClick}
+    >
       <FaBook className="text-xl" />
       <span className="font-bold text-md">{t("Docs")}</span>
     </Button>
--- src/services/externalLink/external.link.service.ts
+++ src/services/externalLink/external.link.service.ts
@@ -1,16 +1,18 @@
 export class ExternalLinkService {
-    static openInNewTab(url: string) {
-      if (!url.startsWith("http")) {
-        console.warn("Invalid external URL:", url);
-        return;
-      }
-  
-      window.open(url, "_blank", "noopener,noreferrer");
+  static openInNewTab(url: string) {
+    if (!url.startsWith("http")) {
+      console.warn("Invalid external URL:", url);
+      return;
     }
-  
-    static reportBug() {
-      this.openInNewTab("https://github.com/Loadforge/SITE/issues/new");
-    }
-  
+
+    window.open(url, "_blank", "noopener,noreferrer");
   }
-  
+
+  static reportBug() {
+    this.openInNewTab("https://github.com/Loadforge/SITE/issues/new");
+  }
+
+  static openDocs() {
+    this.openInNewTab("https://github.com/loadforge/docs");
+  }
+}
